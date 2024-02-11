7e60830ef4ef1b72fb52afe1f9ac3d47e74a35b8
Irina Ichim <onlyirina7@gmail.com>
2024-02-11 03:12:23 +0100

    agregando validaciones js para formulario

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("Fem Coders Club funciona")
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+        const nameInput = document.getElementById("name");
+        const name = nameInput.value;
+
+        const regex = /^[A-Za-z]+$/;
+
+        if (!regex.test(name)) {
+            document.getElementById("nameError").style.display = "block";
+            event.preventDefault();
+        } else {
+            document.getElementById("nameError").style.display = "none";
+            window.location.href = "message.html";
+        }
+    });
+});
