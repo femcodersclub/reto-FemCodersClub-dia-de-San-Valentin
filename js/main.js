731cd8324cefdb9e6d07a0586ccecb2c78e006d6
Irina Ichim <onlyirina7@gmail.com>
2024-02-11 03:40:19 +0100

    generador de mensajes aleatorios

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-console.log("Fem Coders Club funciona")
+console.log("Fem Coders Club funciona");
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("myForm").addEventListener("submit", function(event) {
-        const nameInput = document.getElementById("name");
-        const name = nameInput.value;
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("myForm")
+    .addEventListener("submit", function (event) {
+      const nameInput = document.getElementById("name");
+      const name = nameInput.value;
 
-        const regex = /^[A-Za-z]+$/;
+      const regex = /^[A-Za-z]+$/;
 
-        if (!regex.test(name)) {
-            document.getElementById("nameError").style.display = "block";
-            event.preventDefault();
-        } else {
-            document.getElementById("nameError").style.display = "none";
-            window.location.href = "message.html";
-        }
+      if (!regex.test(name)) {
+        document.getElementById("nameError").style.display = "block";
+        event.preventDefault();
+      } else {
+        document.getElementById("nameError").style.display = "none";
+
+        const randomMessage = getRandomMessage();
+        sessionStorage.setItem("randomMessage", randomMessage);
+        window.location.href = "html/message.html";
+      }
     });
 });
---
+++ js/showRandomMessage.js
@@ -0,0 +1,39 @@
+// Necesito más mensajes
+function getRandomMessage() {
+  const messages = [
+    "Eres especial y único/a en mi vida.",
+    "Tu amor ilumina mis días más oscuros.",
+    "Aprecio cada momento que compartimos juntos/as.",
+    "Tu amistad es un regalo que atesoro.",
+    "Que este día esté lleno de amor y alegría para ti.",
+    "Gracias por ser una parte tan importante de mi mundo.",
+    "Celebremos el amor y la amistad en todas sus formas.",
+  ];
+
+  const storedMessages =
+    JSON.parse(sessionStorage.getItem("storedMessages")) || [];
+
+  const availableMessages = messages.filter(
+    (message) => !storedMessages.includes(message)
+  );
+
+  if (availableMessages.length === 0) {
+    sessionStorage.removeItem("storedMessages");
+    return getRandomMessage();
+  }
+
+  const randomIndex = Math.floor(Math.random() * availableMessages.length);
+  const randomMessage = availableMessages[randomIndex];
+
+  storedMessages.push(randomMessage);
+  sessionStorage.setItem("storedMessages", JSON.stringify(storedMessages));
+
+  return randomMessage;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const randomMessageDiv = document.getElementById("randomMessage");
+  if (randomMessageDiv) {
+    randomMessageDiv.innerText = getRandomMessage();
+  }
+});
