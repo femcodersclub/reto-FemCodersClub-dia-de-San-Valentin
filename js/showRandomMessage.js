74025fdbcb7a6c06815e14ec1c0f692ead56ae14
Irina Ichim <onlyirina7@gmail.com>
2024-02-11 14:00:58 +0100

    agregando mensajes random

--- js/showRandomMessage.js
+++ js/showRandomMessage.js
@@ -1,13 +1,17 @@
 // Necesito más mensajes
 function getRandomMessage() {
   const messages = [
-    "Eres especial y único/a en mi vida.",
     "Tu amor ilumina mis días más oscuros.",
-    "Aprecio cada momento que compartimos juntos/as.",
-    "Tu amistad es un regalo que atesoro.",
-    "Que este día esté lleno de amor y alegría para ti.",
+    "En este día de amor y amistad, te envío mis mejores deseos. ¡Feliz San Valentín!",
+    "Tu amistad es un regalo valioso. ¡Feliz Día de San Valentín!",
+    "Que este día esté lleno de amor y alegría para ti¡Feliz San Valentin!",
+    "Que este día esté lleno de afecto y buenos momentos. ¡Feliz San Valentín para ti!",
     "Gracias por ser una parte tan importante de mi mundo.",
-    "Celebremos el amor y la amistad en todas sus formas.",
+    "Celebremos el amor en todas sus formas. ¡Feliz San Valentín a una persona increíble!",
+    "En este día especial, quiero recordarte lo increíble que eres. ¡Feliz San Valentín!",
+    "Que este día esté lleno de momentos especiales y cariño. ¡Feliz San Valentín para ti!",
+    "Cada momento a tu lado es una aventura mágica. Feliz Día de San Valentín.",
+    "¡Feliz Día de San Valentín! Que este día esté lleno de amor, amistad y felicidad."
   ];
 
   const storedMessages =
